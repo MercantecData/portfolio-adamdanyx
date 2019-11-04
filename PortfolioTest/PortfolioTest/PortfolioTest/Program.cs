58fd04d7b7577bea0522de5b72301fdd275f891d
Mads Bock Christensen <bockchristensen@gmail.com>
2019-11-04 12:19:33 +0100

    Added two projects

---
+++ PortfolioTest/PortfolioTest/PortfolioTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PortfolioTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int[] CountUpTo(int n)
+        {
+            int arraySize = n + 1;
+            int[] a = new int[arraySize];
+            int i = 0;
+            while(i < n)
+            {
+                a[i] = i + 1;
+                i += 1;
+            }
+
+            return a;
+        }
+    }
+}
