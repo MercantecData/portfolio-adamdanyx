0a6cb764609d0114e1a153df330d879f600654a4
Mads Bock Christensen <bockchristensen@gmail.com>
2019-11-06 10:21:54 +0100

    Made progress on #1

--- LibrarySystem/LibrarySystem/LibrarySystem/Program.cs
+++ LibrarySystem/LibrarySystem/LibrarySystem/Program.cs
@@ -6,7 +6,7 @@ namespace LibrarySystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Library!");
         }
     }
 }
